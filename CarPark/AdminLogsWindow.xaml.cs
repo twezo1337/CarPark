c3810890c5b70d8a467d5d1ee3b7339246d5b1a4
twezo1337 <46641501+twezo1337@users.noreply.github.com>
2023-05-30 03:01:20 +0700

    completeLog

--- CarPark/AdminLogsWindow.xaml.cs
+++ CarPark/AdminLogsWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Fernet;
+using MaterialDesignColors;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,6 +28,8 @@ namespace CarPark
             InitializeComponent();
         }
 
+        int linesCount = 0;
+
         private void back_btn_Click(object sender, RoutedEventArgs e)
         {
             Close();
@@ -32,9 +37,95 @@ namespace CarPark
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            StreamReader sr = new StreamReader("login_info.txt");
-            string encryptData = sr.ReadToEnd();
-            richtb_logs.AppendText(encryptData);
+            btn_all.IsEnabled = false;
+            StreamReader srKey = new StreamReader("logKey.key");
+            string strKey = srKey.ReadToEnd();
+            var byteKey = strKey.UrlSafe64Decode();
+            srKey.Close();
+
+            StreamReader srLogs = new StreamReader("LogFile.log");
+            while (!srLogs.EndOfStream)
+            {
+                string encryptLog = srLogs.ReadLine();
+                var decoded64 = SimpleFernet.Decrypt(byteKey, encryptLog, out var timestamp);
+                var decoded = decoded64.UrlSafe64Encode().FromBase64String();
+                tb_logs.AppendText(decoded + "\n");
+                linesCount++;
+
+            }
+            srLogs.Close();  
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            
+            DateTime? date = datePicker.SelectedDate;
+            string[] logLines = tb_logs.Text.Split("\n");
+            if (date != null && logLines.Length > 0 && logLines.Length != linesCount)
+            {
+                tb_logs.Clear();
+
+                string dateStr = "";
+                for (int j = 0; j < logLines.Length; j++)
+                {
+                    for (int i = 0; i < logLines[0].Split(" ").Length; i++)
+                    {
+                        if (logLines[0].Split(" ")[i] == "at")
+                        {
+                            dateStr += logLines[0].Split(" ")[i + 1];
+                            dateStr += " ";
+                            dateStr += logLines[0].Split(" ")[i + 2];
+                            break;
+                        }
+                    }
+                    if (dateStr != "" && DateOnly.FromDateTime(Convert.ToDateTime(dateStr)) == DateOnly.FromDateTime(Convert.ToDateTime(date)))
+                    {
+                        tb_logs.AppendText(logLines[j] + "\n");
+                    }
+                    dateStr = "";
+                }
+            } 
+            else
+            {
+                StreamReader srKey = new StreamReader("logKey.key");
+                string strKey = srKey.ReadToEnd();
+                var byteKey = strKey.UrlSafe64Decode();
+                srKey.Close();
+                StreamReader srLogs = new StreamReader("LogFile.log");
+                while (!srLogs.EndOfStream)
+                {
+                    string encryptLog = srLogs.ReadLine();
+                    var decoded64 = SimpleFernet.Decrypt(byteKey, encryptLog, out var timestamp);
+                    var decoded = decoded64.UrlSafe64Encode().FromBase64String();
+                    tb_logs.AppendText(decoded + "\n");
+
+                }
+                srLogs.Close();
+            }
+            btn.IsEnabled = false;
+            btn_all.IsEnabled = true;
+        }
+
+        private void btn_all_Click(object sender, RoutedEventArgs e)
+        {
+                tb_logs.Clear();
+                StreamReader srKey = new StreamReader("logKey.key");
+                string strKey = srKey.ReadToEnd();
+                var byteKey = strKey.UrlSafe64Decode();
+                srKey.Close();
+
+                StreamReader srLogs = new StreamReader("LogFile.log");
+                while (!srLogs.EndOfStream)
+                {
+                    string encryptLog = srLogs.ReadLine();
+                    var decoded64 = SimpleFernet.Decrypt(byteKey, encryptLog, out var timestamp);
+                    var decoded = decoded64.UrlSafe64Encode().FromBase64String();
+                    tb_logs.AppendText(decoded + "\n");
+
+                }
+                srLogs.Close();
+                btn_all.IsEnabled = false;
+                btn.IsEnabled = true;
         }
     }
 }
